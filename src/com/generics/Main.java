2e23c1b2dbf41130b5ffaf0db6fab115b3150af0
sovisrushain <sovisrushain@gmail.com>
2022-03-15 12:29:27 +0530

    Generics demo

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -2,6 +2,10 @@ package com.generics;
 
 public class Main {
     public static void main(String[] args) {
-
+        Student<String, Course> student1 = new Student<>();
+        student1.setName("student1");
+        Course course1 = new Course(001, "Computer Science");
+        student1.setCourse(course1);
+        System.out.println(student1.toString());
     }
 }
--- src/com/generics/Student.java
+++ src/com/generics/Student.java
@@ -1,4 +1,38 @@
 package com.generics;
 
-public class Student {
+public class Student<String, Course> {
+    private java.lang.String name;
+    private Course course;
+
+    public Student() {
+    }
+
+    public Student(java.lang.String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Student{" +
+                "name=" + name +
+                ", course=" + course +
+                '}';
+    }
 }
